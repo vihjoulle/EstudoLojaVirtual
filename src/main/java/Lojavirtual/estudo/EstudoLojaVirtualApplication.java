2465ba873c6f9f4f8ecf95ae813147ab7a74c70f
victool <victordeghandi@gmail.com>
2023-09-09 18:19:27 -0300

    commit inicial projeto loja java

---
+++ src/main/java/Lojavirtual/estudo/EstudoLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package Lojavirtual.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoLojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoLojaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lojavirtual/estudo/EstudoLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package Lojavirtual.estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoLojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
